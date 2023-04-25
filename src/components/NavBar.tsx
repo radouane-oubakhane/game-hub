c9905d4203ab278abb6f697eea62d0d223d2b7d9
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-25 16:43:48 +0100

    Build a Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import {HStack, Image, Text} from "@chakra-ui/react";
+import logo from '../assets/logo.jpg'
+
+
+
+function NavBar() {
+    return (
+        <HStack>
+            <Image src={logo} boxSize="60px" />
+            <Text>Navbar</Text>
+        </HStack>
+    )
+
+}
+
+export default NavBar
