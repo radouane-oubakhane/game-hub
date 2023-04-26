d6e27c2f262aeea57ba78a3c7eff96e7340dad98
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-26 11:51:38 +0100

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,14 +24,20 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games, setGames] = useState([] as Game[]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] =  useState(false);
 
     useEffect(() => {
+        setLoading(true);
         const controller = new AbortController();
         apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                setGames(res.data.results)
+                setLoading(false);
+            })
             .catch(err => {
                 if (err instanceof CanceledError) return;
                 setError(err.message)
+                setLoading(false);
             });
 
         return () => {
@@ -39,7 +45,7 @@ const useGames = () => {
         }
     }, [])
 
-    return {games, error}
+    return {games, error, isLoading}
 
 }
 
