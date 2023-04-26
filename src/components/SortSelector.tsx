3eb244ab151f3b485d7a95c0b86825b7324524b3
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-26 18:32:50 +0100

    Handle games with no image

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -10,9 +10,10 @@ function PlatformSelector({onSelectSortOrder, sortOrder}: Props) {
     const sortOrders = [
         {value: '', label: 'Relevance'},
         {value: '-added', label: 'Date added'},
-        {value: 'name', label: 'Release date'},
-        {value: '-released', label: 'Popularity'},
-        {value: '-rating', label: 'Average rating'},
+        {value: 'name', label: 'Name'},
+        {value: '-released', label: 'Release date'},
+        {value: '-metacritic', label: 'Popularity'},
+        {value: '-rating', label: 'Average rating'}
   ]
     const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
     return (
