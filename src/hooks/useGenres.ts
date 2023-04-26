c48ab7303ca55685d04b020efc59eab936114d0d
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-26 12:19:51 +0100

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import apiClient from "../services/api-client";
+import {CanceledError} from "axios";
+import {Game} from "./useGames";
+
+interface Genre {
+    id: number;
+    name: string;
+}
+
+interface FetchGenresResponse {
+    count: number;
+    results: Game[];
+}
+
+function UseGenres() {
+    const [genres, setGenres] = useState([] as Genre[]);
+    const [error, setError] = useState('');
+    const [isLoading, setLoading] =  useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const controller = new AbortController();
+        apiClient.get<FetchGenresResponse>('/genres', {signal: controller.signal})
+            .then(res => {
+                setGenres(res.data.results)
+                setLoading(false);
+            })
+            .catch(err => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
+                setLoading(false);
+            });
+
+        return () => {
+            controller.abort();
+        }
+    }, [])
+
+    return {genres, error, isLoading}
+
+}
+
+export default UseGenres;
